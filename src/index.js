e7e46e9c0aab38b95aa8a4b76473f43b35534140
Unknown <letrungkien0210@gmail.com>
2017-09-05 06:02:02 +0700

    Make js, css.
    Issues: not implement the UI yet.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'font-awesome/css/font-awesome.css';
+import 'ionicons/dist/css/ionicons.css';
+import 'admin-lte/dist/css/AdminLTE.css';
+import 'icheck/skins/square/blue.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
