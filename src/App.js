1c980ba72070a090a71864bac89633d5b9b9a548
Unknown <letrungkien0210@gmail.com>
2017-09-06 19:22:30 +0700

    Removed some unnecessary comments
    Removed some unuse css.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import { Switch, Route, Link } from 'react-router-dom';
-import logo from './logo.svg';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import Login from './login';
 
@@ -8,10 +7,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {/*<ul>*/}
-          {/*<li><Link to='/'>Home</Link></li>*/}
-          {/*<li><Link to='/login'>Login</Link></li>*/}
-        {/*</ul>*/}
         <Switch>
           <Route path='/login' component={Login}/>
         </Switch>
