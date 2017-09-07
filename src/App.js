2794d8e3be9a899e17a313963d753777c63f93e2
Jakub Knejzlik <jakub.knejzlik@gmail.com>
2017-09-07 20:54:05 +0200

    Update route to use root path

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react';
-import { Switch, Route } from 'react-router-dom';
-import './App.css';
-import Login from './login';
+import React, { Component } from "react";
+import { Switch, Route } from "react-router-dom";
+import "./App.css";
+import Login from "./login";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Switch>
-          <Route path='/login' component={Login}/>
+          <Route path="/" component={Login} />
         </Switch>
       </div>
     );
