a1ecfa0022b6a18fd0953b78d5eb22ef74570e06
Unknown <letrungkien0210@gmail.com>
2017-09-13 07:05:14 +0700

    make the callout for error message

--- src/login.js
+++ src/login.js
@@ -29,8 +29,10 @@ class Login extends Component {
     this.handleSubmit = this.handleSubmit.bind(this);
     
     this.state = {
-      username: '',
-      password: ''
+      username: 'test@novacloud.cz',
+      password: 'test',
+      getTokenError: false,
+      getTokenErrorMessage: ''
     }
   }
   
@@ -46,27 +48,34 @@ class Login extends Component {
     e.preventDefault();
     const options = {
       method: "POST",
-      uri: this.props.tokenUrl,
-      formData: {
+      url: this.props.tokenUrl,
+      headers: {
+        "content-type":"application/json"
+      },
+      body: {
         grant_type: "password",
         username: this.state.username,
         password: this.state.password,
-        scope: this.props.scope
+        audience: this.props.audience,
+        scope: this.props.scope,
+        client_id: this.props.client_id
       },
-      headers: {
-        "content-type":"application/x-www-form-urlencoded"
-      }
-    }
+      json: true
+    };
+    
     rp(options)
-      .then(token => {
-        localStorage.set("token", token);
+      .then(response => {
+        const token = response.access_token;
+        const token_type = response.token_type;
+        this.changeStatusGetTokenError(false);
+        localStorage.setItem("token", `${token_type} ${token}`);
         if(this.props.redirectUrl){
           window.location.href = `${this.props.redirectUrl}?access_token=${token}`;
         }
       })
       .catch( error => {
         console.error(error);
-        alert(error.message);
+        this.changeStatusGetTokenError(true, error.error.error_description);
       });
   }
   
@@ -75,11 +84,11 @@ class Login extends Component {
       return (
         <form onSubmit={this.handleSubmit}>
           <div className="form-group has-feedback">
-            <input type="text" className="form-control" placeholder="Username" name="username" onChange={this.handleChange}></input>
+            <input type="text" className="form-control" placeholder="Username" name="username" onChange={this.handleChange} value={this.state.username}></input>
             <span className="glyphicon glyphicon-user form-control-feedback"></span>
           </div>
           <div className="form-group has-feedback">
-            <input type="password" className="form-control" placeholder="Password" name="password" onChange={this.handleChange}></input>
+            <input type="password" className="form-control" placeholder="Password" name="password" onChange={this.handleChange} value={this.state.password}></input>
             <span className="glyphicon glyphicon-lock form-control-feedback"></span>
           </div>
           <div className="row">
@@ -131,6 +140,29 @@ class Login extends Component {
     return authProviders;
   }
 
+  renderCalloutError(){
+    if(this.state.getTokenError){
+      return (
+        <span>
+          <br/>
+          <div className="callout callout-danger">
+            <h4>Error</h4>
+            <p>{this.state.getTokenErrorMessage}</p>
+          </div>
+        </span>
+      );
+    } else {
+      return '';
+    }
+  }
+  
+  changeStatusGetTokenError(getTokenError, getTokenErrorMessage = ''){
+    this.setState({
+      getTokenError,
+      getTokenErrorMessage
+    })
+  }
+  
   render() {
     let flagShowOR =
       !this.props.userCredentialsEnabled ||
@@ -149,6 +181,7 @@ class Login extends Component {
               {this.props.boxMessage}
             </p>
             {this.renderUserCredentialsEnabled()}
+            {this.renderCalloutError()}
             <div className="social-auth-links text-center">
               {!flagShowOR ? <p>- OR -</p> : ""}
               {this.renderAuthProviders()}
@@ -173,10 +206,11 @@ Login.defaultProps = {
   userCredentialsEnabled: true,
   authProviders: ["facebook", "google", "gitlab"],
   authUrl: "",
-  tokenUrl: "",
+  tokenUrl: "https://novacloud.eu.auth0.com/oauth/token",
   redirectUrl: "",
-  client_id: "",
-  scope: ""
+  client_id: "mhjoWWbu6ByAfXKt73jMqNMjoFpA8vyQ",
+  scope: "email",
+  audience: "https://novacloud.eu.auth0.com/api/v2/",
 }
 
 export default Login;
