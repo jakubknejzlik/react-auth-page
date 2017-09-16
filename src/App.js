9a63705632a0fefc84915baec2afe42f974b74fd
Unknown <letrungkien0210@gmail.com>
2017-09-16 15:12:44 +0700

    Create example with secured page.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
 import { Switch, Route } from "react-router-dom";
 import "./App.css";
-import Login from "./login";
+import Login from "./example/login";
+import Content from "./example/content";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Switch>
+          <Route path="/content" component={Content}/>
           <Route path="/" component={Login} />
         </Switch>
       </div>
